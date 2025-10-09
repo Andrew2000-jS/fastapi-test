3f688c50bdad208266965ab69de9ac02bf9e4cd3
andres.arevalo <andres.arevalo@stockviz.com>
2025-10-09 12:06:00 -0400

    upd criteria and add cache

--- app/common/criteria.py
+++ app/common/criteria.py
@@ -17,7 +17,6 @@ class LogicalOperator(Enum):
     OR = "$or"
     NOR = "$nor"
 
-
 class OrderBy(Enum):
     """
     Enumeration for sort order directions.
@@ -28,7 +27,6 @@ class OrderBy(Enum):
     """
     ASC = 1
     DESC = -1
-
 
 class SortDTO(CommonBaseModel):
     """
